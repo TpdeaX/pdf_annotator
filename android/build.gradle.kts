1af26282140855c3008eb347996f1a13effc7a39
TpdeaX <84187852+TpdeaX@users.noreply.github.com>
2026-07-27 13:05:11 -0500

    Force compileSdk to 35 for old plugins like isar_flutter_libs

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -30,6 +30,7 @@ subprojects {
             if (androidExt.namespace == null) {
                 androidExt.namespace = project.group.toString()
             }
+            androidExt.compileSdk = 35
         }
     }
     if (project.state.executed) {
